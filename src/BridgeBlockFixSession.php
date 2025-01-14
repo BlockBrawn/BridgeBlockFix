4d32e3d63b87cab0ae25c6ec9f4d0b1d1d348e3e
GameParrot <85067619+GameParrot@users.noreply.github.com>
2025-01-13 22:08:50 -0500

    Add files via upload

---
+++ src/BridgeBlockFixSession.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GameParrot\BridgeBlockFix;
+
+use pocketmine\math\Facing;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\types\inventory\PredictedResult;
+use pocketmine\network\mcpe\protocol\types\inventory\UseItemTransactionData;
+use pocketmine\network\PacketHandlingException;
+use pocketmine\player\Player;
+use function array_push;
+use function in_array;
+use function microtime;
+
+class BridgeBlockFixSession {
+	/** @var \WeakMap<Player, BridgeBlockFixSession> */
+	private static \WeakMap $sessions;
+
+	/** @var \WeakReference<Player> */
+	private \WeakReference $player;
+
+	protected float $lastRightClickTime = 0.0;
+	protected ?UseItemTransactionData $lastRightClickData = null;
+
+	public function __construct(Player $player) {
+		$this->player = \WeakReference::create($player);
+	}
+
+	public static function get(Player $player) : self {
+		if (!isset(self::$sessions)) {
+			self::$sessions = new \WeakMap();
+		}
+		return self::$sessions[$player] ??= new self($player);
+	}
+
+	public function handleClickBlockTransaction(UseItemTransactionData $data) : void {
+		$this->player->get()->selectHotbarSlot($data->getHotbarSlot());
+		//TODO: start hack for client spam bug
+		$clickPos = $data->getClickPosition();
+		$spamBug = ($this->lastRightClickData !== null &&
+			microtime(true) - $this->lastRightClickTime < 0.1 && //100ms
+			$this->lastRightClickData->getPlayerPosition()->distanceSquared($data->getPlayerPosition()) < 0.00001 &&
+			$this->lastRightClickData->getBlockPosition()->equals($data->getBlockPosition()) &&
+			$this->lastRightClickData->getClickPosition()->distanceSquared($clickPos) < 0.00001 && //signature spam bug has 0 distance, but allow some error
+			$data->getClientInteractPrediction() === PredictedResult::FAILURE
+		);
+		//get rid of continued spam if the player clicks and holds right-click
+		$this->lastRightClickData = $data;
+		$this->lastRightClickTime = microtime(true);
+		if ($spamBug) {
+			return;
+		}
+		//TODO: end hack for client spam bug
+
+		self::validateFacing($data->getFace());
+
+		$blockPos = $data->getBlockPosition();
+		$vBlockPos = new Vector3($blockPos->getX(), $blockPos->getY(), $blockPos->getZ());
+		if (!$this->player->get()->interactBlock($vBlockPos, $data->getFace(), $clickPos) && !$this->isFailedPrediction($data)) {
+			$this->onFailedBlockAction($vBlockPos, $data->getFace());
+		}
+		return;
+	}
+
+	private static function validateFacing(int $facing) : void {
+		if (!in_array($facing, Facing::ALL, true)) {
+			throw new PacketHandlingException("Invalid facing value $facing");
+		}
+	}
+
+	private function isFailedPrediction(UseItemTransactionData $data) : bool {
+		return $data->getClientInteractPrediction() === PredictedResult::FAILURE;
+	}
+
+	private function onFailedBlockAction(Vector3 $blockPos, ?int $face) : void {
+		if ($blockPos->distanceSquared($this->player->get()->getLocation()) < 10000) {
+			$blocks = $blockPos->sidesArray();
+			if ($face !== null) {
+				$sidePos = $blockPos->getSide($face);
+
+				/** @var Vector3[] $blocks */
+				array_push($blocks, ...$sidePos->sidesArray()); //getAllSides() on each of these will include $blockPos and $sidePos because they are next to each other
+			} else {
+				$blocks[] = $blockPos;
+			}
+			foreach ($this->player->get()->getWorld()->createBlockUpdatePackets($blocks) as $packet) {
+				$this->player->get()->getNetworkSession()->sendDataPacket($packet);
+			}
+		}
+	}
+}
---
+++ src/Main.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GameParrot\BridgeBlockFix;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\handler\ItemStackContainerIdTranslator;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\network\mcpe\protocol\types\inventory\UseItemTransactionData;
+use pocketmine\network\PacketHandlingException;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use function count;
+
+class Main extends PluginBase implements Listener {
+	public function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function handleInventoryTransaction(InventoryTransactionPacket $packet, Player $player) : bool {
+		if ($packet->trData instanceof UseItemTransactionData && $packet->trData->getActionType() === UseItemTransactionData::ACTION_CLICK_BLOCK) {
+			$inventoryManager = $player->getNetworkSession()->getInvManager();
+			if (!$inventoryManager) {
+				return false;
+			}
+
+			if (count($packet->trData->getActions()) > 50) {
+				throw new PacketHandlingException("Too many actions in inventory transaction");
+			}
+			if (count($packet->requestChangedSlots) > 10) {
+				throw new PacketHandlingException("Too many slot sync requests in inventory transaction");
+			}
+
+			$inventoryManager->setCurrentItemStackRequestId($packet->requestId);
+			$inventoryManager->addRawPredictedSlotChanges($packet->trData->getActions());
+
+			BridgeBlockFixSession::get($player)->handleClickBlockTransaction($packet->trData);
+
+			$inventoryManager->syncMismatchedPredictedSlotChanges();
+
+			//requestChangedSlots asks the server to always send out the contents of the specified slots, even if they
+			//haven't changed. Handling these is necessary to ensure the client inventory stays in sync if the server
+			//rejects the transaction. The most common example of this is equipping armor by right-click, which doesn't send
+			//a legacy prediction action for the destination armor slot.
+			foreach ($packet->requestChangedSlots as $containerInfo) {
+				foreach ($containerInfo->getChangedSlotIndexes() as $netSlot) {
+					[$windowId, $slot] = ItemStackContainerIdTranslator::translate($containerInfo->getContainerId(), $inventoryManager->getCurrentWindowId(), $netSlot);
+					$inventoryAndSlot = $inventoryManager->locateWindowAndSlot($windowId, $slot);
+					if ($inventoryAndSlot !== null) { //trigger the normal slot sync logic
+						$inventoryManager->onSlotChange($inventoryAndSlot[0], $inventoryAndSlot[1]);
+					}
+				}
+			}
+
+			$inventoryManager->setCurrentItemStackRequestId(null);
+
+			return true;
+		}
+		return false;
+	}
+
+	public function onDataPacket(DataPacketReceiveEvent $event) : void {
+		if ($event->getPacket()->pid() === InventoryTransactionPacket::NETWORK_ID) {
+			/** @var InventoryTransactionPacket */
+			$packet = $event->getPacket();
+			$player = $event->getOrigin()->getPlayer();
+			if ($player !== null && $this->handleInventoryTransaction($packet, $player)) {
+				$event->cancel();
+			}
+		}
+	}
+}
